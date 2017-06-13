2f4c93260036156ea64fcd0b01b8e2e0847d47f8
cvmars <cvmars@sina.com>
2017-06-13 16:08:19 +0800

    创建项目目录

--- youxiake-guide/app/src/main/java/com/youxiake_guide/ui/activity/MainActivity.java
+++ youxiake-guide/app/src/main/java/com/youxiake_guide/ui/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.youxiake_guide;
+package com.youxiake_guide.ui.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.youxiake_guide.R;
 
 public class MainActivity extends AppCompatActivity {
 
